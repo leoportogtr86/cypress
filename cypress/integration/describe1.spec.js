3f87a0898f65f5ecba7b5f00fd4909799ff9f40d
leoportogtr86 <leoporto_studio@hotmail.com>
2021-02-10 08:04:48 -0300

    criando grupos de teste com describe para agrupar 1 ou varios testes internos

---
+++ cypress/integration/describe1.spec.js
@@ -0,0 +1,33 @@
+/// <references types="cypress"/>
+
+
+
+
+//criando um teste
+it('Teste externo', () => {
+
+    console.log('segundo teste com cypress')
+
+
+})
+
+
+describe('Grupo de testes', () => {
+
+    it('Teste interno 1', () => {
+        console.log('Teste interno 1')
+
+    })
+
+    it('Teste interno 2', () => {
+        console.log('Teste interno 2')
+
+    })
+
+    it('Teste interno 3', () => {
+        console.log('Teste interno 3')
+
+    })
+
+
+})

5572ae63d2c1d8d3df470b65b16771e5dab5c6ff
leoportogtr86 <leoporto_studio@hotmail.com>
2021-02-16 20:35:50 -0300

    interagindo com botao - conferindo se o value corresponde a uma string passada

--- cypress/integration/interagindo.spec.js
+++ cypress/integration/interagindo.spec.js
@@ -7,4 +7,6 @@ it('Interagindo', () => {
 
     cy.visit('http://wcaquino.me/cypress/componentes.html')
     cy.get('#buttonSimple').click()
+
+    cy.get('#buttonSimple').should('value', 'Obrigado!')
 })
